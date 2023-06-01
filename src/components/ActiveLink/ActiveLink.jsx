f5af29bc7dbc254fbb54969265f79674658f1235
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-06-02 00:28:36 +0600

    Active  Link added

--- src/components/ActiveLink/ActiveLink.jsx
+++ src/components/ActiveLink/ActiveLink.jsx
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import './ActiveLink.css'
-// import { NavLink } from 'react-router-dom';
+import React from 'react';
+import './ActiveLink.css'
+import { NavLink } from 'react-router-dom';
 
-// const ActiveLink = ({ to, children }) => {
-//      return (
-//           <div className='activeLink'>
-//                <NavLink
-//                     to={to}
-//                     className={({ isActive }) => isActive ? "active" : "noActive"}
-//                >
-//                     {children}
-//                </NavLink>
-//           </div>
-//      );
-// };
+const ActiveLink = ({ to, children }) => {
+     return (
+          <div className='activeLink'>
+               <NavLink
+                    to={to}
+                    className={({ isActive }) => isActive ? "active" : "noActive"}
+               >
+                    {children}
+               </NavLink>
+          </div>
+     );
+};
 
-// export default ActiveLink;
+export default ActiveLink;
