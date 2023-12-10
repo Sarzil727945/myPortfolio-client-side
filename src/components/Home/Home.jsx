7902c6e9e1d65e60b1b35c187438a04931a43b88
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-12-10 18:45:02 +0600

    update inspect

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -36,10 +36,13 @@ const Home = () => {
                                                   />
                                              </span></h4>
                                         <p className=' text-muted fw-semibold pe-lg-5 fontSize mx-lg-0 mx-2'>
-                                             I'm a Sarzil Muntaha, Since Now, I have 1 years plus of extensive hands on experience of website development and Software Development. <br />
-                                              I have a high level of personal morals and integrity. I am Goal oriented, self-motivated and committed to the successful outcome of the project. I am willing to work hard and have a great desire to learn. <br />
-                                             I am passionate about building excellent software that improves the lives of those around me.
-                                             Currently working as a Software Engineer at "Urban IT Solution".
+                                             I'm a Sarzil Muntaha. Since Now, I have 1 years plus of extensive hands
+                                             on experience of website development and Software Development. <br /> I
+                                             have a high level of personal morals and integrity. I am Goal oriented,
+                                             self-motivated and committed to the successful outcome of the project.
+                                             I am willing to work hard and have a great desire to learn. <br /> I want to
+                                             create software that improves the lives of those around me. Currently
+                                             working as a Software Engineer at "Urban IT Solution".
                                         </p>
                                         <div className='mt-5'>
                                              <button className='borderNone'>
